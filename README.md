845b76ac69fe9216d072ceb927e27fb86760debb
Miles Glapa-Grossklag <mglapagr@ucsc.edu>
2021-04-18 22:05:19 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Setup
+
+First, create a file `config.py` with the token like below:
+
+```py
+token = "my_secret_token"
+```
+
+Then, to install dependencies, run the following:
+
+```sh
+$ python -m virtualenv venv
+$ source ./venv/bin/activate
+$ pip install -r ./requirements.txt
+```
+
+# Usage
+
+```sh
+$ python main.py
+```
---
+++ main.py
@@ -0,0 +1,36 @@
+import discord
+import re
+from piazza_api import Piazza
+import config
+
+
+class Client(discord.Client):
+    def __init__(self):
+        super(Client, self).__init__()
+
+        # Regex to check if message wants a post
+        self.re = re.compile("p([0-9])+", re.IGNORECASE)
+
+        # Piazza setup
+        p = Piazza()
+        p.user_login()
+        self.network = p.network("kmfs2bmdr9syz") # CSE 13S
+
+    async def on_ready(self):
+        print("Connected as {}".format(self.user))
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        match = self.re.search(message.content)
+        if match:
+            post = match.group()
+            await message.reply("https://piazza.com/class/kmfs2bmdr9syz?cid={}".format(post, post))
+
+def main():
+    client = Client()
+    client.run(config.token)
+
+if __name__ == "__main__":
+    main()
