cbef40d027e32ae462cebb8902a497435b54b01f
Ben Grant <benjamin.j.grant@gmail.com>
2021-04-19 03:56:37 -0700

    Allow user who was replied to to delete the bot's message

--- main.py
+++ main.py
@@ -20,7 +20,25 @@ class Client(discord.Client):
         match = self.re.search(message.content)
         if match:
             post = match.group(2)
-            await message.reply("https://piazza.com/class/kmfs2bmdr9syz?cid={}".format(post, post))
+            reply = await message.reply(("Piazza @{}: https://piazza.com/class/kmfs2bmdr9syz?cid={}\n"
+                    "(<@{}> can delete by reacting)").format(post, post, message.author.id))
+            await reply.add_reaction("🗑️")
+
+    async def on_raw_reaction_add(self, payload):
+        if payload.user_id == self.user.id:
+            # ignore our own reactions
+            return
+
+        # only wastebasket emoji reactions since that's what the bot reacts with
+        if payload.emoji.name == "🗑️":
+            channel = self.get_channel(payload.channel_id)
+            message = await channel.fetch_message(payload.message_id)
+            # is it our message?
+            if message.author == self.user:
+                # is the reaction from the user who sent the original message?
+                # check by seeing if they're mentioned as the bot mentions the user who sent it
+                if payload.user_id in [user.id for user in message.mentions]:
+                    await message.delete()
 
 def main():
     client = Client()
